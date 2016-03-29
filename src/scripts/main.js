f9a10e49053d32ff329f7e7cf6b6432ee2875383
Mikk Meelak <mikk@platvorm.ee>
2016-03-29 18:51:07 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,58 +22,63 @@ $(document).ready(function() {
 	resizeTopImage();
 	resizeArchitectImage();
 
+	if ($('iframe.sc-widget').length) {
 
-	var player = SC.Widget($('iframe.sc-widget')[0]);
-	var pOffset = $('.player').offset();
-	var pWidth = $('.player').width();
-	var scrub;
+		var player = SC.Widget($('iframe.sc-widget')[0]);
 
-	$('.player-button').on('click', function(e) {
-		e.preventDefault();
+		if (player) console.log('yo');
 
-		player.toggle();
-	})
+		var pOffset = $('.player').offset();
+		var pWidth = $('.player').width();
+		var scrub;
 
-	// Set info on load
-	player.bind(SC.Widget.Events.READY, function() {
-		setInfo();
-	});
+		$('.player-button').on('click', function(e) {
+			e.preventDefault();
 
-	player.bind(SC.Widget.Events.PLAY_PROGRESS, function(e) {
-		// if (e.relativePosition < 0.003) { setInfo(); }
-		$('.position').css('width', (e.relativePosition * 100) + "%");
-	});
+			player.toggle();
+		})
 
-	$('.player').mousemove(function(e) {
-		scrub = (e.pageX - pOffset.left);
-	});
-
-	$('.player').click(function() {
-		$('.position').css('width', scrub + "px");
-		var seek = player.duration * (scrub / pWidth);
-
-		player.seekTo(seek);
-
-		$('.player-seek').html(msToTime(seek));
-	});
-
-	function setInfo() {
-		player.getCurrentSound(function(song) {
-			console.log(song);
-
-			$('.player-info').html(song.title);
-
-			player.current = song;
+		// Set info on load
+		player.bind(SC.Widget.Events.READY, function() {
+			setInfo();
 		});
 
-		player.getDuration(function(value) {
-			player.duration = value;
-			$('.player-duration').html(msToTime(player.duration));
+		player.bind(SC.Widget.Events.PLAY_PROGRESS, function(e) {
+			// if (e.relativePosition < 0.003) { setInfo(); }
+			$('.position').css('width', (e.relativePosition * 100) + "%");
 		});
 
-		player.isPaused(function(bool) {
-			player.getPaused = bool;
+		$('.player').mousemove(function(e) {
+			scrub = (e.pageX - pOffset.left);
 		});
+
+		$('.player').click(function() {
+			$('.position').css('width', scrub + "px");
+			var seek = player.duration * (scrub / pWidth);
+
+			player.seekTo(seek);
+
+			$('.player-seek').html(msToTime(seek));
+		});
+
+		function setInfo() {
+			player.getCurrentSound(function(song) {
+				console.log(song);
+
+				$('.player-info').html(song.title);
+
+				player.current = song;
+			});
+
+			player.getDuration(function(value) {
+				player.duration = value;
+				$('.player-duration').html(msToTime(player.duration));
+			});
+
+			player.isPaused(function(bool) {
+				player.getPaused = bool;
+			});
+		}
 	}
 });
 
